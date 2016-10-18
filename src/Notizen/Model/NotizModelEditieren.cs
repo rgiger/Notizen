ae210fb46ac7b9668acc377c59d0072fb77fe765
r.giger <r.giger@BILL>
2016-10-18 16:05:14 +0200

    Partial für Erstellen und  Editieren erstellt.

--- src/Notizen/Model/NotizModelEditieren.cs
+++ src/Notizen/Model/NotizModelEditieren.cs
@@ -4,7 +4,7 @@ using Notizen.DbModel;
 
 namespace Notizen.Model
 {
-    public class NotizModelEditieren : NotizModelBase
+    public class NotizModelEditieren : NotizModelErstellen
     {
         public NotizModelEditieren()
         {
@@ -13,20 +13,20 @@ namespace Notizen.Model
         public NotizModelEditieren(NotizDbModel u) : base(u)
         {
             Id = u.Id;
-            if (u.ErledigtBis != null)
-            {
-                ErledigtBisDatum = u.ErledigtBis.Value.Date;
-                ErledigtBisZeit = u.ErledigtBis.Value.TimeOfDay;
-            }
+            //if (u.ErledigtBis != null)
+            //{
+            //    ErledigtBisDatum = u.ErledigtBis.Value.Date;
+            //    ErledigtBisZeit = u.ErledigtBis.Value.TimeOfDay;
+            //}
         }
         
         public int Id { get; set; }
 
         //public int Id { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime? ErledigtBisDatum { get; set; }
+        //[DataType(DataType.Date)]
+        //public DateTime? ErledigtBisDatum { get; set; }
 
-        [DataType(DataType.Time)]
-        public TimeSpan? ErledigtBisZeit { get; set; }
+        //[DataType(DataType.Time)]
+        //public TimeSpan? ErledigtBisZeit { get; set; }
     }
 }
