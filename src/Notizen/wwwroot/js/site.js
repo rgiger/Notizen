54ece2649e0520a25f0610954c994eb82fac0481
Michael Leu <m1leu@hsr.ch>
2016-10-20 22:47:07 +0200

    Abbrechen-Buttons und Datenverlust-Check bei Forms

--- src/Notizen/wwwroot/js/site.js
+++ src/Notizen/wwwroot/js/site.js
@@ -1,3 +1,20 @@
-Array.from(document.getElementsByClassName("relativedate")).forEach(function(element) {
+Array.from(document.getElementsByClassName("relativedate")).forEach(function (element) {
   element.textContent = moment(element.textContent, "DD.MM.YYYY HH:mm").fromNow();
 });
+
+var forms = Array.from(document.getElementsByTagName('form'));
+if (forms.length) {
+  var warneVorDatenverlust = function (event) {
+    if (Array.from(document.getElementsByTagName('input')).filter(function (element) {
+      return element.type == "text" && typeof element.value != "undefined" && element.value != element.defaultValue;
+    }).length) {
+      event.preventDefault();
+    }
+  }
+  window.addEventListener("beforeunload", warneVorDatenverlust);
+  forms.forEach(function (element) {
+    element.addEventListener("submit", function () {
+      window.removeEventListener("beforeunload", warneVorDatenverlust);
+    });
+  });
+}
